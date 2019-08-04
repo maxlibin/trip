246557bc0408e03719e88dbc6a874df1a2e926ca
maxlibin <me@maxlibin.com>
2019-08-04 21:50:02 +0800

    add store

--- mobile/tripdeer-trip-planner/reducers/Trips.js
+++ mobile/tripdeer-trip-planner/reducers/Trips.js
@@ -2,7 +2,7 @@ const initialState = {};
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case GET_REPOS:
+    case "GET_VIEWS":
       return { ...state, loading: true };
     default:
       return state;
